98e6ebfe50f29fbe1b324c213028da6c06df3561
elena <chayori@icloud.com>
2022-10-02 21:48:01 +0300

    Задача решена

---
+++ from_to/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая на вход принимает число N, а на выходе показывает 
+все целые числа от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" */
+
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+int num = int.Parse(number); 
+
+for (int i = - num; i <= num; i++)
+{
+    Console.WriteLine($"{i}");
+}
