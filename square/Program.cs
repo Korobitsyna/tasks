4648968eae6f216232e091bc9b32b0436d22df57
elena <chayori@icloud.com>
2022-10-02 20:47:54 +0300

    Задача решена

---
+++ square/Program.cs
@@ -0,0 +1,10 @@
+﻿// написать программу, которая на вход принимает число и выдает его квадрат.
+
+Console.Write("Введите число:");
+string numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr); // Переменную string to int
+int result = number * number;
+
+Console.WriteLine($"{number} -> {result}");
+
