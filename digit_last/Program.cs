966f1777b7cee073cddf4b2e0cc0326898c8c43d
elena <chayori@icloud.com>
2022-10-02 21:55:55 +0300

    Задача решена

---
+++ digit_last/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход трехзначное число
+и на выходе показывает последнюю цифру этого числа.
+456 -> 6 */
+
+Console.WriteLine("Введите трехзначное число ");
+string number = Console.ReadLine();
+int num = int.Parse(number); 
+
+if (num < 1000 && num > 99)
+{
+    Console.WriteLine(num%10);
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
