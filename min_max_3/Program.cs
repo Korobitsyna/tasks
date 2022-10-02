3e5f0268729b1bcda6a47a8653eb83788ad99df3
elena <chayori@icloud.com>
2022-10-02 22:17:02 +0300

    Задача решена

---
+++ min_max_3/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.WriteLine("Введите первое число:");
+string firstnumber = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+string secondnumber = Console.ReadLine();
+Console.WriteLine("Введите третье число:");
+string thirdnumber = Console.ReadLine();
+
+int firstnum = int.Parse(firstnumber);
+int secondnum = int.Parse(secondnumber);
+int thirdnum = int.Parse(thirdnumber);
+
+
+if (firstnum > secondnum && firstnum > thirdnum)
+{
+    Console.WriteLine($"{firstnum}, {secondnum}, {thirdnum} -> max = {firstnum}");
+}
+else if (secondnum > firstnum && secondnum > thirdnum)
+{
+    Console.WriteLine($"{firstnum}, {secondnum}, {thirdnum} -> max = {secondnum}");
+}
+else
+{
+    Console.WriteLine($"{firstnum}, {secondnum}, {thirdnum} -> max = {thirdnum}");
+}
