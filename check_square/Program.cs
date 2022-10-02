c7d5cd53a0d5931f6a225f67d4976f508016a967
elena <chayori@icloud.com>
2022-10-02 21:10:58 +0300

    Задача решена

---
+++ check_square/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Написать программу, которая принимает два числа и проверяет, является ли первое число
+квадратом второго.
+пример a = 25, b = 5 -> да
+пример b = 2, b = 10 -> нет */
+
+Console.WriteLine("Введите первое число:");
+string firstnumber = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+string secondnumber = Console.ReadLine();
+
+int firstnum = int.Parse(firstnumber); 
+int secondnum = int.Parse(secondnumber); 
+
+if (secondnum * secondnum == firstnum)
+    {
+        Console.WriteLine($"a = {firstnumber}, b= {secondnumber} -> да");
+    }
+else
+ {
+        Console.WriteLine($"a = {firstnumber}, b = {secondnumber} -> нет");
+    }
+
